3061b36066d3dd068063785dcbac3a1167b9e3d6
Brad Fach <bfach@uwaterloo.ca>
2013-11-18 17:41:08 -0500

    nigga

--- src/main/java/com/boogle/marketbuddy/dao/PortfolioDAOImpl.java
+++ src/main/java/com/boogle/marketbuddy/dao/PortfolioDAOImpl.java
@@ -2,13 +2,14 @@ package com.boogle.marketbuddy.dao;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -27,8 +28,8 @@ public class PortfolioDAOImpl implements PortfolioDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
 
-	Logger LOG = Logger.getLogger(PortfolioDAOImpl.class);
-	
+	private static final Logger LOG = LoggerFactory.getLogger(PortfolioDAOImpl.class);
+	 
 	@Override
 	public void writePortfolio(Portfolio portfolio, String username) {
 		portfolio.setUserId(userDAO.retrieveUserId(username));
